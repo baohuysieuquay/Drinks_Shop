d1f531b675d3f9c6a9ddc1225b5cf5a535da734c
Nguyen, Gia Bao <baohuysieuquay@gmail.com>
2023-05-19 17:03:06 +0200

    Add files via upload
    Add Manager

---
+++ source/chars/Manager.java
@@ -0,0 +1,31 @@
+package Chars;
+
+public class Manager {
+	private int totalOrders = 0;
+	private int correctMadeOrders = 0;
+	
+	
+	
+	public int getTotalOrders() {
+		return totalOrders;
+	}
+
+
+
+	public int getCorrectMadeOrders() {
+		return correctMadeOrders;
+	}
+
+
+
+	public void receiveOrder(Customer customer) {
+		System.out.println("Ordering...");
+		for (int i = 0; i < customer.getOrder().length; i++) {
+			System.out.printf("Drink number %d\n", (i + 1));
+			customer.orderTrinks();
+			customer.orderToppings();
+			System.out.println(customer.getOrder()[i].str());
+			totalOrders++;
+		}
+	}
+}
