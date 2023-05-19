7590fd900ac3139aa0b3079303ee749d6634d308
Nguyen, Gia Bao <baohuysieuquay@gmail.com>
2023-05-19 17:02:06 +0200

    Add files via upload
    Add main

---
+++ source/Main.java
@@ -0,0 +1,19 @@
+import Assets.*;
+import Chars.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer1 = new Customer();
+		
+		Manager manager1 = new Manager();
+		
+		manager1.receiveOrder(customer1);
+		
+		while (true) {
+			
+		}
+
+	}
+
+}
